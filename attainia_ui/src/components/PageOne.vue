19d94b751cecd00810dd55e89f118d010660d78f
Herbert Gutierrez <herbert.gutierrez@outlook.com>
2020-03-12 02:38:03 -0500

    issues with axios

--- attainia_ui/src/components/PageOne.vue
+++ attainia_ui/src/components/PageOne.vue
@@ -3,3 +3,25 @@
     <h1>Table 1 component</h1>
   </div>
 </template>
+
+<script>
+// import axios from 'axios'; 
+export default {
+  created(){
+    // axios.get('http://localhost:8000/users/', {headers: {"Access-Control-Allow-Origin":"http://localhost:8000", "Access-Control-Allow-Credentials": "true"}})
+    // .then(res =>{
+    //   console.log(res.data);
+    // })
+    // .catch(err => console.log(err));
+
+    fetch("http://localhost:8000/users/").then(function (response) {
+      console.log(response);
+            // return response.json();
+    });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
